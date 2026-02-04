55ee6bc0aa7ca9c3317298d70fa1e888dba29c20
Aaron Meche <aaronmeche18@icloud.com>
2026-02-04 17:00:16 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<!-- created by Aaron Meche -->
+<script lang='ts'>
+
+</script>
+
+<!--  -->
+
+<div class='page'>
+    hi
+</div>
+
+<!--  -->
+
+<style>
+    
+
+</style>
---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,16 @@
+<!-- created by Aaron Meche -->
+<script lang='ts'>
+
+</script>
+
+<!--  -->
+
+<div class='page'>
+    Account Page
+</div>
+
+<!--  -->
+
+<style>
+
+</style>
