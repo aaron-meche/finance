d7724568ee70a39d338f451b23d3b3f6e4d0f5fe
Aaron Meche <aaronmeche18@icloud.com>
2026-02-05 14:25:55 -0600

    firebase app hosting prep tweaks, added apphosting.yaml

---
+++ server.js
@@ -0,0 +1,3 @@
+// server.js (root of project)
+import { handler } from './build/handler.js';
+export default handler;
--- src/lib/plaid.js
+++ src/lib/plaid.js
@@ -1,13 +1,13 @@
 // $lib/plaid.js
 import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
-import { PLAID_CLIENT_ID, PLAID_SECRET } from "$env/static/private";
+import { env } from '$env/dynamic/private';
 
 export const plaidClient = new PlaidApi({
     basePath: PlaidEnvironments.sandbox,
     baseOptions: {
         headers: {
-        'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
-        'PLAID-SECRET': PLAID_SECRET,
+            'PLAID-CLIENT-ID': env.PLAID_CLIENT_ID,
+            'PLAID-SECRET': env.PLAID_SECRET,
         },
     },
 });
