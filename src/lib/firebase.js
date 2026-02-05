637d56a889d5140c8b6b521e9e4fc19dd30040d8
Aaron Meche <aaronmeche18@icloud.com>
2026-02-05 12:42:23 -0600

    firebase init

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLkrrpsR85yH0X_5zFh7sj5n2Fgaz067E",
+  authDomain: "finance-c24f5.firebaseapp.com",
+  projectId: "finance-c24f5",
+  storageBucket: "finance-c24f5.firebasestorage.app",
+  messagingSenderId: "485405299981",
+  appId: "1:485405299981:web:cedd65540ac8c293200c44"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
